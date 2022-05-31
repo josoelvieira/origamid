d4a1a0247a302d6db7ebed1e3777391ff3247007
Josoel Vieira <cvl.jv1@gmail.com>
2022-05-31 19:35:02 -0300

    add helment para alt de title e metas do head

--- router/src/Components/Home.jsx
+++ router/src/Components/Home.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Helmet from 'react-helmet'
+
 
 const Home = () => {
   return (
     <div>
+      <Helmet>
+                <meta charSet="utf-8" />
+                <title>Home</title>
+                <meta name="CVL developer" description="CVL developer desenvolvendo com amor e react" />
+      </Helmet>
       <h1>Home</h1>
       <p>Esta e a home</p>
       <span><Link to="produto/notebook">Notebook</Link></span> | <span><Link to="produto/smartphone">Smartphone</Link></span>
+      
     </div>
   )
 }
