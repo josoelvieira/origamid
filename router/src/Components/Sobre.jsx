f90489f4ff5ce7b3c0d2d2738c816b1564d58f93
Josoel Vieira <cvl.jv1@gmail.com>
2022-05-31 19:58:23 -0300

    inic exercicio router usando router e helment

--- router/src/Components/Sobre.jsx
+++ router/src/Components/Sobre.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 
 const Sobre = () => {
   return (
     <div>
+      <Helmet>
+        <title>CVL developer | Sobre</title>
+      </Helmet>
         <h1>Sobre</h1>
         <p>Esta e a pagina sobre</p>
     </div>
