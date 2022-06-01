315c21a7ce2656faed86d4878ff72aaaf08430e9
Josoel Vieira <cvl.jv1@gmail.com>
2022-06-01 09:01:40 -0300

    adicionando tailwind ao projeto

--- exercicio-router/src/App.jsx
+++ exercicio-router/src/App.jsx
@@ -1,9 +1,16 @@
 import './App.css'
+import { Routes, Route, BrowserRouter} from 'react-router';
+import Header from './Components/Header';
+import Contato from './Components/Contato';
 
 function App() {
   return (
     <div className="App">
-      test
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Produtos/>} />
+        <Route path="/contato" element={<Contato/>} />
+      </Routes>
     </div>
   )
 }
